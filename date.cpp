9a0ed72ed0465eef4f5da1a6a82db5b0f716b473
breno12filipe <breno.filipe@sempreceub.com>
2022-12-31 14:10:32 -0300

    finishing menu

---
+++ date.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "date.h"
+
+void Date::calculateNumbers(float firstIn, float firstOut, float lastIn, float lastOut){
+    std::cout << "--------" << std::endl;
+    std::cout << firstIn << std::endl;
+    std::cout << firstOut << std::endl;
+    std::cout << lastIn << std::endl;
+    std::cout << lastOut << std::endl;
+    std::cout << "--------" << std::endl;
+}
---
+++ date.h
@@ -0,0 +1,7 @@
+class Date {
+    public:
+        // dummy implementation
+        Date();
+        ~Date();
+        static void calculateNumbers(float firstIn, float firstOut, float lastIn, float lastOut){};
+};
