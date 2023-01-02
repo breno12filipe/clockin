29a18e1e45fc444eb4b0853057cc901f2af38fa1
breno12filipe <breno.filipe@sempreceub.com>
2023-01-02 17:11:18 -0300

    fixing for error

--- main.cpp
+++ main.cpp
@@ -2,39 +2,35 @@
 #include <map>
 #include <vector>
 #include "utils/colormod.h"
-#include "date.h"
+#include "src/date.h"
 
 void allocateInputValue(std::vector<float> *week, float &input) {
     week->push_back(input);
 }
 
 int main() {
-    int i, j, k, weekNo, monthDay, optionsNo;
-    float firstIn, firstOut, lastIn, lastOut, inputVal;
+    int i, j, weekNo, monthDay;
+    float firstIn, firstOut, lastIn, lastOut;
 
-    std::map<char,int> weekValues;
     color::modifier magenta(color::FG_MAGENTA);
     color::modifier def(color::FG_DEFAULT);
 
     std::cout << magenta << "    (CLOCKIN 0.1)    " << std::endl;
-
     std::cout << def << "    Digite a quantidade de semanas" << std::endl;
-    std::cin >> weekNo;
+    std::cin  >> weekNo;
     std::cout << "    Digite o mes" << std::endl;
-    std::cin >> monthDay;
+    std::cin  >> monthDay;
 
     for (int i = 0; i < weekNo; i++) {
-        for (int j = 0; j < 5; j++) {
-            std::cout << "    Digite a primeira hora de entrada" << std::endl;
-            std::cin  >> firstIn;
-            std::cout << "    Digite a primeira hora de saida" << std::endl;
-            std::cin  >> firstOut;
-            std::cout << "    Digite a ultima hora de entrada" << std::endl;
-            std::cin  >> lastIn;
-            std::cout << "    Digite a ultima hora de saida" << std::endl;    
-            std::cin  >> lastOut;
-            Date::calculateNumbers(firstIn, firstOut, lastIn, lastOut);
-        }
+        std::cout << "    Digite a primeira hora de entrada" << std::endl;
+        std::cin  >> firstIn;
+        std::cout << "    Digite a primeira hora de saida" << std::endl;
+        std::cin  >> firstOut;
+        std::cout << "    Digite a ultima hora de entrada" << std::endl;
+        std::cin  >> lastIn;
+        std::cout << "    Digite a ultima hora de saida" << std::endl;  
+        std::cin  >> lastOut;
+        Date::calculateNumbers(firstIn, firstOut, lastIn, lastOut);
     }
     return 0;
 }
