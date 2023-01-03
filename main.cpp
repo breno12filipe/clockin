93e191af097e867d46a34078c8d4ec5a6dc56dda
breno12filipe <breno.filipe@sempreceub.com>
2023-01-03 01:13:51 -0300

    adjusting main menu

--- main.cpp
+++ main.cpp
@@ -7,10 +7,15 @@
 int main() {
     color::modifier magenta(color::FG_MAGENTA);
     color::modifier def(color::FG_DEFAULT);
+    color::modifier blue(color::FG_BLUE);
+    color::modifier green(color::FG_GREEN);
+    
 
     std::string inputPath, outputPath;
-    std::cout << magenta << "    (CLOCKIN 0.1)    " << std::endl;
-    std::cout << "Digite o local de origem do arquivo:" << std::endl;
+    std::cout << blue << "\n\n       ###############" << std::endl;
+    std::cout << green << "        (CLOCKIN 1.0)" << std::endl;
+    std::cout << blue << "       ###############" << std::endl;
+    std::cout << def     << "Digite o local de origem do arquivo:" << std::endl;
     std::cin  >> inputPath;
     std::cout << "Digite o local de destino do arquivo:" << std::endl;
     std::cin  >> outputPath;
